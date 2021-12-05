51602b0a2869ccdc875f010ad23ab68f189e7bfc
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-12-05 11:45:51 -0300

    Java

--- src/arquitetura/integracao/src/main/java/br/com/alura/leilao/login/LoginPage.java
+++ src/arquitetura/integracao/src/main/java/br/com/alura/leilao/login/LoginPage.java
@@ -1,38 +1,33 @@
 package br.com.alura.leilao.login;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import br.com.alura.leilao.PageObject;
+import br.com.alura.leilao.leiloes.LeiloesPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginPage {
+public class LoginPage extends PageObject {
 
-    private static final String URL_LOGIN = "http://localhost:8080/login?";
+    private static final String URL_LOGIN = "http://localhost:8080/login";
 
 
-    private WebDriver browser;
 
     public LoginPage() {
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-        this.browser = new ChromeDriver();
+        super(null);
         this.browser.navigate().to(URL_LOGIN);
     }
 
 
-    public void fechar() {
-        this.browser.quit();
-    }
 
     public void preencheFormularioDeLogin(String username, String password) {
         browser.findElement(By.id("username")).sendKeys(username);
         browser.findElement(By.id("password")).sendKeys(password);
     }
 
-    public void efetuaLogin() {
+    public LeiloesPage efetuaLogin() {
         browser.findElement(By.id("login-form")).submit();
+        return new LeiloesPage(browser);
     }
 
     public boolean isPaginaDeLogin() {
