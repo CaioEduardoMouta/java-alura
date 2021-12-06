7dbb0d63750b84bcc0294b1826498a4caa4ef6e0
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-12-05 21:52:31 -0300

    java

--- src/arquitetura/integracao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/arquitetura/integracao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -3,19 +3,37 @@ package br.com.alura.leilao.acceptance.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.it.Quando;
 import io.cucumber.java.pt.Entao;
 import org.junit.Assert;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PropondoLanceSteps {
 
     private Lance lance;
     private Leilao leilao;
-    private Lance lance10;
-    private Lance lance15;
+    private ArrayList<Lance> lista;
+
+
+    @Before
+    public void setup() {
+        this.lista = new ArrayList<Lance>();
+        leilao = new Leilao("Tablet XPTO");
+
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("after");
+    }
 
     @Dado("um lance valido")
     public void dado_um_lance_valido() {
@@ -35,22 +53,62 @@ public class PropondoLanceSteps {
         Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
     }
 
-    @Dado("varios lances validos")
-    public void varios_lances_validos() {
-        Usuario usuario1 = new Usuario("fulano");
-        lance10 = new Lance(usuario1, BigDecimal.TEN);
-        Usuario usuario2 = new Usuario("beltrano");
-        lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+//    @Dado("varios lances validos")
+//    public void varios_lances_validos() {
+//        Usuario usuario1 = new Usuario("fulano");
+//        lance10 = new Lance(usuario1, BigDecimal.TEN);
+//        Usuario usuario2 = new Usuario("beltrano");
+//        lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+//    }
+
+
+
+    @Dado("um lance de {double} reais do usuario {string}")
+    public void um_lance_de_reais_do_usuario_fulano(Double valor, String nomeUsuario) {
+        Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
+        lista.add(lance);
     }
 
     @Quando("propoe o lance")
     public void propoe_o_lance() {
-        leilao = new Leilao("Tablet XPTO");
-        leilao.propoe(lance);
+        this.lista.forEach(lance -> leilao.propoe(lance));
+
     }
     @Entao("os lances sao aceitos")
     public void os_lances_sao_aceitos() {
-
+        Assert.assertEquals(this.lista.size(),leilao.getLances().size());
+        Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+        Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(1).getValor());
     }
+
+
+    @Dado("um lance de {double} reais e do usuario {string}")
+    public void um_lance_de_reais(Double valor, String nomeUsuario) {
+        //System.out.println(nomeUsuario);
+        this.lance = new Lance(new BigDecimal(valor));
+    }
+
+    @Entao("o lance nao e aceito")
+    public void o_lance_nao_e_aceito() {
+        Assert.assertEquals(0,leilao.getLances().size());
+    }
+    @Dado("dois lances")
+    public void dois_lances(DataTable dataTable) {
+        List<Map<String, String>> valores = dataTable.asMaps();
+        for(Map<String, String> mapa : valores) {
+            String valor = mapa.get("valor");
+            String nome = mapa.get("nomeUsuario");
+            Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+            lista.add(lance);
+        }
+    }
+
+
+    @Entao("o segundo lance nao e aceito")
+    public void o_segundo_lance_nao_e_aceito() {
+        Assert.assertEquals(1,leilao.getLances().size());
+        Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+    }
+
 
 }
--- src/arquitetura/integracao/src/test/resources/features/propondo_lances.feature
+++ src/arquitetura/integracao/src/test/resources/features/propondo_lances.feature
@@ -8,6 +8,26 @@ Cenario:  Propondo um unico lance valido
     Entao o lance aceito
 
     Cenario:  Propondo varios lances validos
-        Dado varios lances validos
+        Dado um lance de 10.0 reais do usuario "fulano"
+        E um lance de 15.0 reais do usuario "beltrano"
         Quando propoe o lance
         Entao os lances sao aceitos
+
+    Esquema do Cenario: Propondo um lance invalido
+        Dado um lance de <valor> reais e do usuario '<nomeUsuario>'
+        Quando propoe ao leilao
+        Entao o lance nao e aceito
+
+ Exemplos:
+    |valor | nomeUsuario |
+    |0     | beltrano    |
+    |-1    | cigano     |
+
+  Cenario:  Propondo uma sequencia de lances
+    Dado dois lances
+      |valor    | nomeUsuario |
+      | 10.0    | beltrano    |
+      | 15.0    | beltrano    |
+    E um lance de 15.0 reais do usuario "fulano"
+    Quando propoe o lance
+    Entao o segundo lance nao e aceito
