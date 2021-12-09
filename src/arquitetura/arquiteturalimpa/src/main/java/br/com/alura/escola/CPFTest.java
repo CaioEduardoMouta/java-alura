3726ab62adb8bb5f46f324285751a235ec48d6b5
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-12-08 22:49:52 -0300

    Java

---
+++ src/arquitetura/arquiteturalimpa/src/main/java/br/com/alura/escola/CPFTest.java
@@ -0,0 +1,20 @@
+package br.com.alura.escola;
+
+import br.com.alura.escola.aluno.Email;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CPFTest {
+
+    @Test
+    void naoDeveriaCriarCPFInvalido() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Email(null));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Email(""));
+        assertThrows(IllegalArgumentException.class,
+                () -> new Email("12345678900"));
+    }
+
+}
